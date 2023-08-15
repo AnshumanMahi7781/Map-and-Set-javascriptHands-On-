fd6b73939ebcfc8240a88e7caba9436e9c10ddd2
Anshuman Kumar <mahianshuman491@gmail.com>
2023-08-16 01:42:36 +0530

    Completed

--- map.js
+++ map.js
@@ -1,29 +1,23 @@
 
-
-//! Q2.	"Problem:
-// You are given a string (STR) of length N, you have to print the count of all alphabet.(using maps)
-
-// Input:
-// abcadeecfb
-
-// Output:
-// a=2
-// b=2
-// c=2
-// d=1
-// e=2
-// f=1
-
 let ans = new Map();
+
 for (let char of str) {
+
     if (ans.has(char)) {
+
        ans.set(char, ans.get(char)+1)
+
     } else {
+
         ans.set(char, 1);
+
     }
+
 }
 
 for(let [key, value] of ans){
+
     console.log(`${key}=${value}`);
+    
 }
 
--- set.js
+++ set.js
@@ -1,18 +1,10 @@
-//! Question-1 "Problem:
-// You are given a string (STR) of length N, consisting of only the lower case English alphabet.
-// Your task is to remove all the duplicate occurrences of characters in the string.
-
-// Input:
-// abcadeecfb
-
-// Output:
-// abcdef"
 
 let str = "abcadeecfb";
 let UniqueStr = "";
 let StrArr = str.split('');
 let UniqueArr = new Set(StrArr);
-for(let values of UniqueArr){
-    UniqueStr +=values;
+for (let values of UniqueArr) {
+    UniqueStr += values;
 }
+
 console.log(UniqueStr); // abcdef
